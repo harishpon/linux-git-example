790f545b448d3ef090e882a9212a34cbb1663da8
harishpon <harish.ponnappan1@gmail.com>
2022-01-01 17:21:40 +1100

    hello.c ask user for their name and store it in variable

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+	char name[1024];
+
 	printf("hello\n");
 	printf("my name is harish\n");
+	printf("what is your name?")
+	
+	//scan in the name 
+	fgets(string, 1024, stdin);
+	prinf("\n");
+	
 	return 0;
 }
